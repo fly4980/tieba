c9f63a9635d90de71d94acbf3d598296b555834d
ltf <ltf11@foxmail.com>
2018-07-27 13:20:13 +0800

    注释

--- tieba_spider.py
+++ tieba_spider.py
@@ -9,18 +9,18 @@ class TiebaSpider:
         self.headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
 
-    def get_url_list(self):
+    def get_url_list(self):  # 构造url列表
         url_list = []
         for i in range(1000):
             url_list.append(self.url_temp.format(i * 50))
         return url_list
 
-    def parse_url(self, url):
+    def parse_url(self, url):  # 发送请求获取响应
         print(url)
         response = requests.get(url,headers=self.headers)
         return response.content.decode()
 
-    def save_html(self, html_str, page_num):
+    def save_html(self, html_str, page_num):  # 保存获取的html页面
         file_path = "{}-第{}页.html".format(self.tieba_name, page_num)
         with open(file_path,"w",encoding="utf-8") as f:
             f.write(html_str)
